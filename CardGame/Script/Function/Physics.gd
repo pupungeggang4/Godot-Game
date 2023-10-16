18ca58ea0ff524feed9c986fe8bb6fac4efce944
pupungeggang2 <kyongkyong289@outlook.kr>
2023-10-16 22:56:02 +0900

    0.0.2

--- CardGame/Script/Function/Physics.gd
+++ CardGame/Script/Function/Physics.gd
@@ -1,11 +1,7 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func point_inside_rect(x, y, r1, r2, r3, r4):
+	return x > r1 and x < r1 + r3 and y > r2 and y < r2 + r4
+	
+func point_inside_rect_array(x, y, rect):
+	return x > rect[0] and x < rect[0] + rect[2] and y > rect[1] and y < rect[1] + rect[3]
--- CardGame/Script/Input.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var mouse_down = false
